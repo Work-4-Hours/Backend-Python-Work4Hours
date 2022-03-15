ec6e1ba0ffe1eb100273ce4ba135230724c68090
David Torres <dmtg16@hotmail.com>
2022-03-15 17:59:00 -0500

    Se corrigen errores

--- models/users.py
+++ models/users.py
@@ -2,12 +2,51 @@ from utils.db import db
 from sqlalchemy import Table, Column, Integer, ForeignKey, String
 from sqlalchemy.orm import relationship, backref
 
-class Users(db.model):
+class Departament(db.Model):
+    __tablename__ = 'departamentos'
+    iddepartamento = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable= False)
+
+    def __init__(self,nombre):
+        self.nombre= nombre
+
+
+class City(db.Model):
+    __tablename__ = 'ciudades'
+    idciudad = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable= False)
+    iddepartamento = db.Column(db.Integer, ForeignKey('departamentos.iddepartamento'),nullable=False)
+    departamentos = relationship(Departament, backref=backref('departamentos', uselist=True))
+
+    def __init__(self,nombre,iddepartamento):
+        self.nombre=nombre
+        self.iddepartamento=iddepartamento
+
+
+class Statuses(db.Model):
+    __tablename__ = 'estados'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre_reporte = db.Column(db.String(50), nullable=False)
+
+    def __init__(self,nombre_reporte):
+        self.nombre_reporte=nombre_reporte
+
+
+class Rol(db.Model):
+    __tablename__ = 'roles'
+    idrol = db.Column(db.Integer, primary_key=True)
+    nombrerol = db.Column(db.String(15), nullable=False)
+
+    def __init__(self,nombrerol):
+        self.nombrerol=nombrerol
+
+
+class Users(db.Model):
     __tablename__ = 'usuarios'
     idusuario = db.Column(db.Integer, primary_key=True)
     nombres = db.Column(db.String(60), nullable= False)
     apellidos = db.Column(db.String(60), nullable= False)
-    celular = db.Columnn(db.String(30), nullable= False)
+    celular = db.Column(db.String(30), nullable= False)
     direccion = db.Column(db.String(500), nullable= False)
     correo = db.Column(db.String(500), nullable = False)
     contrasenna = db.Column(db.String(45), nullable= False)
@@ -34,40 +73,16 @@ class Users(db.model):
         self.estado = estado
 
 
-class Categories(db.model):
-    __tablename__="categorias"
+class Categories(db.Model):
+    __tablename__='categorias'
     idcategoria = db.Column(db.String, primary_key=True)
     nombrecateg = db.Column(db.String(30), nullable=True)
-
 
     def __init__(self,nombrecateg):
         self.nombrecateg=nombrecateg
 
 
-
-class City(db.model):
-    __tablename__ = 'ciudades'
-    idciudad = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(50), nullable= False)
-    iddepartamento = db.Column(db.Integer, ForeignKey('departamentos.iddepartamento'),nullable=False)
-    departamentos = relationship(De, backref=backref('departamentos', uselist=True))
-
-    def __init__(self,nombre,iddepartamento):
-        self.nombre=nombre
-        self.iddepartamento=iddepartamento
-
-
-class Departament(db.model):
-    __tablename__ = 'departamentos'
-    iddepartamento = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(50), nullable= False)
-
-
-    def __init__(self,nombre):
-        self.nombre= nombre
-
-
-class Hall(db.model):
+class Hall(db.Model):
     __tablename__ = 'sala'
     idsala = db.Column(db.Integer, primary_key=True)
     fechainicio = db.Column(db.Date, nullable= False)
@@ -83,7 +98,7 @@ class Hall(db.model):
         self.horafin=horafin
 
 
-class Messages(db.model):
+class Messages(db.Model):
     __tablename__ = 'mensajes'
     idmensaje = db.Column(db.Integer, primary_key=True)
     mensaje = db.Column(db.String(1000), nullable=False)
@@ -100,7 +115,7 @@ class Messages(db.model):
         self.idusuario = idusuario
 
 
-class Report(db.model):
+class Report(db.Model):
     __tablename__ = 'reportes'
     idreporte = db.Column(db.Integer, primary_key=True)
     nombrereporte = db.Column(db.String(14), nullable=False)
@@ -109,31 +124,7 @@ class Report(db.model):
         self.nombrereporte=nombrereporte
 
 
-class Rol(db.model):
-    __tablename__ = 'roles'
-    idrol = db.Column(db.Integer, primary_key=True)
-    nombrerol = db.Column(db.String(15), nullable=False)
-
-
-    def __init__(self,nombrerol):
-        self.nombrerol=nombrerol
-
-
-class Services_reports(db.model):
-    __tablename__ = 'servicio_reportes'
-    id = db.Column(db.Integer, primary_key=True)
-    idreporte = db.Column(db.Integer,ForeignKey('reportes.idreporte'), nullable=False)
-    reportes = relationship(Report, backref=backref('servicio_reportes'), uselist=True)
-    idservicio = db.Column(db.Integer, ForeignKey('servicios.idservicio'),nullable=False)
-    servicios = relationship(Services, backref=backref('servicio_reportes'), uselist=True)
-
-
-    def __init__(self, idreporte, idservicio):
-        self.idreporte= idreporte
-        self.idservicio= idservicio
-
-
-class Services(db.model):
+class Services(db.Model):
     __tablename__ = 'servicios'
     idservicio = db.Column(db.Integer, primary_key=True)
     idcategoria = db.Column(db.String(3), ForeignKey('categorias.idcategoria'),nullable=False)
@@ -159,17 +150,21 @@ class Services(db.model):
         self.foto=foto
         self.usuario=usuario
 
-
-class Statuses(db.model):
-    __tablename__ = 'estados'
+class Services_reports(db.Model):
+    __tablename__ = 'servicio_reportes'
     id = db.Column(db.Integer, primary_key=True)
-    nombre_reporte = db.Column(db.String(50), nullable=False)
+    idreporte = db.Column(db.Integer,ForeignKey('reportes.idreporte'), nullable=False)
+    reportes = relationship(Report, backref=backref('servicio_reportes'), uselist=True)
+    idservicio = db.Column(db.Integer, ForeignKey('servicios.idservicio'),nullable=False)
+    servicios = relationship(Services, backref=backref('servicio_reportes'), uselist=True)
 
-    def __init__(self,nombre_reporte):
-        self.nombre_reporte=nombre_reporte
+
+    def __init__(self, idreporte, idservicio):
+        self.idreporte= idreporte
+        self.idservicio= idservicio
 
 
-class Users_hall(db.model):
+class Users_hall(db.Model):
     __tablename__ = 'sala_usuario'
     idsalausuario = db.Column(db.Integer, primary_key=True)
     idsala = db.Column(db.Integer, ForeignKey('sala.idsala'),nullable=False)
@@ -177,15 +172,13 @@ class Users_hall(db.model):
     idusuario = db.Column(db.Integer, ForeignKey('usuarios.idusuario'),nullable=False)
     usuarios = relationship(Users, backref=backref('sala_usuario', uselist=True))
 
-
-
     def __init__(self, idsala, idusuario):
         self.idsala=idsala
         self.idusuario=idusuario
 
 
 
-class User_services(db.model):
+class User_services(db.Model):
     __tablename__ = 'usuario_reportes'
     id = db.Column(db.Integer, primary_key=True)
     idusuario = db.Column(db.Integer, ForeignKey('usuarios.idusuario'),nullable=False)
--- routes/user.py
+++ routes/user.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, json, jsonify, request
+from models.users import Users
 
 user = Blueprint('user_routes', __name__)
 
@@ -8,6 +9,18 @@ def user_login():
     name = userInfo["name"]
     lastName = userInfo["lastName"]
     phoneNumber = userInfo["phoneNumber"]
+    address = userInfo["address"]
+    email = userInfo["email"]
+    password = userInfo["password"]
+    birthDate = userInfo["birthDate"]
+    picture = userInfo["userPic"]
+    city = userInfo["city"]
+    rol = userInfo["rol"]
+    status = userInfo["status"]
+
+    user = Users(name,lastName,phoneNumber,address,email,password,birthDate,picture,city,rol,status)
+
+    print(user)
     
     return "Login"
 
