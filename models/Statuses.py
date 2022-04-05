d71cd861fd83001d0efa8135f9a907d7bf803e85
Luchomunozz <lmmunoz311@misena.edu.co>
2022-04-05 17:13:17 -0500

    Lucho_dev Se agrega el modelo Qualification

--- models/Statuses.py
+++ models/Statuses.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import relationship, backref
 class Statuses(db.Model):
     __tablename__ = 'estados'
     id = db.Column(db.Integer, primary_key=True)
-    nombre_reporte = db.Column(db.String(50), nullable=False)
+    nombre_estado = db.Column(db.String(50), nullable=False)
 
-    def __init__(self,nombre_reporte):
-        self.nombre_reporte=nombre_reporte
+    def __init__(self,nombre_estado):
+        self.nombre_estado=nombre_estado
