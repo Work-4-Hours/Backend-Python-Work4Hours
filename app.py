8b0373028150dc84f0b0c953b0a87efd76599238
David Torres <dmtg16@hotmail.com>
2022-04-11 22:26:28 -0500

    Se modifican los cors

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.after_request
 def after_request(response):
-    response.headers["Access-Control-Allow-Origin"] = "http://localhost" # <- You can change "*" for a domain for example "http://localhost"
+    response.headers["Access-Control-Allow-Origin"] = "*" # <- You can change "*" for a domain for example "http://localhost"
     response.headers["Access-Control-Allow-Credentials"] = "true"
     response.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, PUT, DELETE"
     response.headers["Access-Control-Allow-Headers"] = "*"
